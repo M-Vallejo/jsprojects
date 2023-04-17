8113474591a9818bd2badabde94f07df561fa6df
Michael Vallejo <19641846+M-Vallejo@users.noreply.github.com>
2023-04-17 18:04:37 -0400

    Fix typo

--- salary.js
+++ salary.js
@@ -6,7 +6,7 @@ AFP: 2.87, max: RD$8,954.40
 SFS: 3.04, max: RD$4,742.40
 TSS: 5.91 (AFP + SFS)
 ISR:
-    ​Rentas hasta RD$416,220.00                                 Exento                                                                 |    0          -> 36,863.642
+    ​Rentas hasta RD$416,220.00                                 Exento                                                                  |    0          -> 36,863.642
     ​Rentas desde RD$416,220.01 hasta RD$624,329.00             ​15% del excedente de RD$416,220.01                                     |    36,863.643 -> 58,751.284
     ​Rentas desde RD$624,329.01 hasta RD$867,123.00             ​RD$31,216.00 más el 20% del excedente de RD$624,329.01                 |    58,751.285 -> 86,219.842
     ​Rentas desde  RD$867,123.01 en adelante                    ​RD$79,776.00 más el 25% del excedente de RD$867,123.01                 |    86,219.843 -> ∞
@@ -57,7 +57,7 @@ const formatter = new Intl.NumberFormat('es-DO', {
   maximumFractionDigits: 2
 });
 
-const formatCurrenty = amount => formatter.format(amount);
+const formatCurrency = amount => formatter.format(amount);
 
 const getAFP = amount => {
     const result = amount * AFP.percent/100;
@@ -91,13 +91,13 @@ const calcNetIncome = (amount, printDetail) => {
     const netIncome = amount - totalDiscount;
 
     if(printDetail) {
-        console.log("AFP: " + formatCurrenty(afp));
-        console.log("SFS: " + formatCurrenty(sfs));
-        console.log("TSS: " + formatCurrenty(tss));
-        console.log("ISR: " + formatCurrenty(isr));
-        console.log("Descuento total: " + formatCurrenty(totalDiscount));
-        console.log("Salario neto: " + formatCurrenty(netIncome));
-        console.log("Salario neto quincenal: " + formatCurrenty(netIncome/2));
+        console.log("AFP: " + formatCurrency(afp));
+        console.log("SFS: " + formatCurrency(sfs));
+        console.log("TSS: " + formatCurrency(tss));
+        console.log("ISR: " + formatCurrency(isr));
+        console.log("Descuento total: " + formatCurrency(totalDiscount));
+        console.log("Salario neto: " + formatCurrency(netIncome));
+        console.log("Salario neto quincenal: " + formatCurrency(netIncome/2));
     }
     return {
       afp,
