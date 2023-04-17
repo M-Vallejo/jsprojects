e9f22a7237ac4472c47a0f4ba2bb37dbcd41cdd5
Michael Vallejo <19641846+M-Vallejo@users.noreply.github.com>
2023-04-17 09:19:50 -0400

    add limit to sfs and afp + some code refactor

--- salary.js
+++ salary.js
@@ -2,19 +2,27 @@
 
 /*
 
-AFP: 2.87
-SFS: 3.04
-TSS: 5.91
+AFP: 2.87, max: RD$8,954.40
+SFS: 3.04, max: RD$4,742.40
+TSS: 5.91 (AFP + SFS)
 ISR:
-    ​Rentas hasta RD$416,220.00                                 Exento                                                                 |    36,863.642
+    ​Rentas hasta RD$416,220.00                                 Exento                                                                 |    0          -> 36,863.642
     ​Rentas desde RD$416,220.01 hasta RD$624,329.00             ​15% del excedente de RD$416,220.01                                     |    36,863.643 -> 58,751.284
     ​Rentas desde RD$624,329.01 hasta RD$867,123.00             ​RD$31,216.00 más el 20% del excedente de RD$624,329.01                 |    58,751.285 -> 86,219.842
-    ​Rentas desde  RD$867,123.01 en adelante                    ​RD$79,776.00 más el 25% del excedente de RD$867,123.01                 |    86,219.843
+    ​Rentas desde  RD$867,123.01 en adelante                    ​RD$79,776.00 más el 25% del excedente de RD$867,123.01                 |    86,219.843 -> ∞
 
 */
 
-const AFP = 2.87;
-const SFS = 3.04;
+const AFP = {
+  percent: 2.87,
+  max: 8954.40
+};
+
+const SFS = {
+  percent: 3.04,
+  max: 4742.40
+};
+
 const ISR = [
     {
         min: null,
@@ -42,54 +50,60 @@ const ISR = [
     }
 ];
 
-function getAFP(amount) {
-    let result = amount * AFP/100;
+const formatter = new Intl.NumberFormat('es-DO', {
+  style: 'currency',
+  currency: 'DOP',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2
+});
+
+const formatCurrenty = amount => formatter.format(amount);
+
+const getAFP = amount => {
+    const result = amount * AFP.percent/100;
+    return result > AFP.max ? AFP.max : result;;
+}
+
+const getSFS = amount => {
+    let result = amount * SFS.percent/100;
+    return result > SFS.max ? SFS.max : result;
+}
+
+const getISR = amount => {
+    const yearlyIncome = amount * 12;
+
+    const isrToCompare = ISR.find(x=> (yearlyIncome <= x.max && yearlyIncome >= x.min) 
+                                        || (yearlyIncome >= x.min && !x.max) 
+                                        || (yearlyIncome <= x.min && !x.min));
+
+    const result = (((yearlyIncome - isrToCompare.min) * isrToCompare.percent/100) + isrToCompare.fixed) / 12;
+
     return result;
 }
 
-function getSFS(amount) {
-    let result = amount * SFS/100;
-    return result;
-}
-
-function getTSS(amount) {
-    let afp = getAFP(amount);
-    let sfs = getSFS(amount);
-    let result = afp + sfs;
-
-    return result;
-}
-
-function getISR(amount) {
-    let yearIncome = amount * 12;
-
-    let isrToCompare = ISR.filter(x=> (yearIncome <= x.max && yearIncome >= x.min) 
-                                        || (yearIncome >= x.min && !x.max) 
-                                        || (yearIncome <= x.min && !x.min));
-    let compare = isrToCompare[0];
-    let result = (((yearIncome - compare.min) * compare.percent/100) + compare.fixed) / 12;
-
-    return result;
-}
-
-function CalcNetIncome(amount, printDetail) {
-    let afp = getAFP(amount);
-    let sfs = getSFS(amount);
-    let tss = afp + sfs;
-    let amountWithDiscount = amount - tss;
-    let isr = getISR(amountWithDiscount);
-
-    let totalDiscount = tss + isr;
-    let netIncome = amount - totalDiscount;
+const calcNetIncome = (amount, printDetail) => {
+    const afp = getAFP(amount);
+    const sfs = getSFS(amount);
+    const tss = afp + sfs;
+    const amountWithDiscount = amount - tss;
+    const isr = getISR(amountWithDiscount);
+    const totalDiscount = tss + isr;
+    const netIncome = amount - totalDiscount;
 
     if(printDetail) {
-        console.log("AFP: " + afp);
-        console.log("SFS: " + sfs);
-        console.log("TSS: " + tss);
-        console.log("ISR: " + isr);
-        console.log("Descuento total: " + totalDiscount);
-        console.log("Salario neto: " + netIncome);
-        console.log("Salario neto quincenal: " + netIncome/2);
+        console.log("AFP: " + formatCurrenty(afp));
+        console.log("SFS: " + formatCurrenty(sfs));
+        console.log("TSS: " + formatCurrenty(tss));
+        console.log("ISR: " + formatCurrenty(isr));
+        console.log("Descuento total: " + formatCurrenty(totalDiscount));
+        console.log("Salario neto: " + formatCurrenty(netIncome));
+        console.log("Salario neto quincenal: " + formatCurrenty(netIncome/2));
     }
-    return netIncome;
+    return {
+      afp,
+      sfs,
+      isr,
+      totalDiscount,
+      netIncome
+    };
 }
